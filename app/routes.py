f8ef85b2abe73e77120d4fff3d6ebb5afb7e61a7
yangxiaojia <yangxiaojia@meicai.cn>
2019-02-18 14:43:35 +0800

    blog模板和框架

---
+++ app/routes.py
@@ -0,0 +1,11 @@
+from app import app
+from flask import render_template
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('blog/index.html')
+
+@app.route('/test')
+def test():
+    return render_template('test/test.html')
