d44768e113e29d683229dc5cee086915371c6a05
yangxiaojia <yangxiaojia@meicai.cn>
2019-02-19 19:05:25 +0800

    添加用户密码加密

--- app/models/user.py
+++ app/models/user.py
@@ -1,7 +1,9 @@
 from app import db
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(UserMixin,db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(32), index=True, unique=True)
     password = db.Column(db.String(128)) #hash
@@ -14,3 +16,9 @@ class User(db.Model):
     
     def __repr__(self):
         return '<User {}>'.format(self.username) 
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
