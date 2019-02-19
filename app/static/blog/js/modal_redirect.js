06cbc6ba9bd7d21665211e767538cbcea20fd7a0
yangxiaojia <yangxiaojia@meicai.cn>
2019-02-19 13:51:57 +0800

    修复注册登录模态框跳转闪动问题

---
+++ app/static/blog/js/modal_redirect.js
@@ -0,0 +1,20 @@
+$("#toLogin").click(function(){
+	$("#register").modal("hide");
+	$("#register").on("hidden.bs.modal",function(){
+		$("#login").modal("show");
+		$("#register").off().on("hidden","hidden.bs.modal");
+	})
+})
+
+$("#toReg").click(function(){
+	$("#login").modal("hide");
+	$("#login").on("hidden.bs.modal",function(){
+		$("#register").modal("show");
+		$("#login").off().on("hidden","hidden.bs.modal");
+	})
+})
+
+
+		
+
+
