ffe8b859587eea8cc4f1060bbf864dfc1c1ac9fe
yangxiaojia <yangxiaojia@meicai.cn>
2019-02-20 20:06:08 +0800

    ajax处理form

--- app/models/user.py
+++ app/models/user.py
@@ -1,4 +1,5 @@
 from app import db
+from app import login
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
@@ -22,3 +23,7 @@ class User(UserMixin,db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password, password)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, jsonify
 from app.forms.login_form import LoginForm
+import json
+
 # from app.models.user import User
 
 @app.route('/')
@@ -16,4 +18,23 @@ def test():
 @app.route('/article')
 def article():
     return render_template('blog/article.html')
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+
+    print("test!!!!!!")
+    # username = request.args.get('username')
+    # password = request.args.get('password')
+    data = request.get_data()
+    print(data)
+    print(type(data))
+    json_data = request.get_json()
+    print(json_data)
+    print(type(json_data))
+
+    
+    res = jsonify({'ret': True, 'msg': '登录成功!'})
+    res.status_code = 200
+ 
+    return  res
 
