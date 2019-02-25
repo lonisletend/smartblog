58d071e5d9fa86bb5cb26d880b20ad79d641fd8d
yangxiaojia <yangxiaojia@meicai.cn>
2019-02-25 20:09:06 +0800

    增加注册模块

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,9 @@
 from app import app
 from flask import render_template, request, jsonify
 from app.forms.login_form import LoginForm
+from app.forms.registeration_form import RegistrationForm
 import json
+from flask_login import current_user, login_user
 
 # from app.models.user import User
 
@@ -11,7 +13,8 @@ import json
 @app.route('/index')
 def index():
     loginForm = LoginForm()
-    return render_template('blog/index.html', title="Index", loginForm=loginForm)
+    registrationForm = RegistrationForm()
+    return render_template('blog/index.html', title="Index", loginForm=loginForm, registrationForm=registrationForm)
 
 @app.route('/test')
 def test():
@@ -26,10 +29,10 @@ def article():
 def login():
     username = request.form['username']
     password = request.form['password']
-    print('username={}, password={}'.format(username, password))
+    # print('username={}, password={}'.format(username, password))
     if username=='yxj' and password=='123456':
-        res = jsonify({'status': True, 'msg': '登录成功!'})
+        res = jsonify({'status': True, 'msg': '登录成功，正在跳转...'})
     else:
-        res = jsonify({'status': False, 'msg': '用户名或密码不匹配！'})
+        res = jsonify({'status': False, 'msg': '用户名或密码错误！'})
     return  res
 
