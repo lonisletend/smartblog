f6e6a3476cccfc053cbf4299b3f904306c194d26
yangxiaojia <yangxiaojia@meicai.cn>
2019-03-15 17:23:35 +0800

    完善发布功能，修复若干bug

--- app/routes.py
+++ app/routes.py
@@ -11,6 +11,7 @@ from app.models.tag import Tag
 from app.models.relation import Relation
 from app import db
 from datetime import datetime
+import copy
 
 # from app.models.user import User
 
@@ -21,17 +22,59 @@ from datetime import datetime
 def index():
     loginForm = LoginForm()
     registrationForm = RegistrationForm()
-    return render_template('blog/index.html', title="Index", loginForm=loginForm, registrationForm=registrationForm)
+    arts = Article.query.filter_by(status=1, is_deleted=0).order_by(Article.created.desc()).all()  #查出所有可见(1)文章+倒序
+    articleList = []
+    for art in arts:
+        artdict = {'id':0, 'title':'', 'text':'', 'date':'', 'author':'', 'cateName':'', 'views':0, 'isTopping':0}
+        artdict['id'] = art.id
+        artdict['title'] = art.title
+        artdict['text'] = art.text[:200]
+        artdict['date'] = art.created.strftime("%Y-%m-%d")
+        user = User.query.filter_by(id=art.user_id).first()
+        artdict['author'] = user.username
+        artdict['views'] = art.views
+        cate = Category.query.filter_by(id=art.cate_id).first()
+        artdict['cateName'] = cate.name
+        artdict['isTopping'] = art.is_topping
+        articleList.append(copy.deepcopy(artdict))
+
+    return render_template('blog/index.html', title="Index", loginForm=loginForm, 
+                            registrationForm=registrationForm, articleList=articleList)
 
 @app.route('/test')
 def test():
     return render_template('test/test.html')
 
-@app.route('/article')
-def article():
+@app.route('/article/<artid>')
+def article(artid):
     loginForm = LoginForm()
     registrationForm = RegistrationForm()
-    return render_template('blog/article.html', title="Article", loginForm=loginForm, registrationForm=registrationForm)
+    art = Article.query.filter_by(id=artid).first()
+    article = {'id':0, 'title':'', 'text':'', 'date':'', 'author':'', 'cateName':'', 'tagNames':[], 'views':0, 'isTopping':0}
+    article['id'] = art.id
+    article['title'] = art.title
+    article['text'] = art.text
+    article['date'] = art.created.strftime("%Y-%m-%d")
+    user = User.query.filter_by(id=art.user_id).first()
+    article['author'] = user.username
+    article['views'] = art.views+1
+    cate = Category.query.filter_by(id=art.cate_id).first()
+    article['cateName'] = cate.name
+    # artdict['tags'] = []
+    res = Relation.query.filter_by(art_id=art.id).all()
+    for re in res:
+        tag = Tag.query.filter_by(id=re.tag_id).first()
+        article['tagNames'].append(copy.deepcopy(tag.name))
+    article['isTopping'] = art.is_topping
+    
+    # 阅读数+1
+    Article.query.filter_by(id=artid).update({
+        'views': art.views+1
+    })
+    db.session.commit()
+
+    return render_template('blog/article.html', title="Article", loginForm=loginForm, 
+                            registrationForm=registrationForm, article=article)
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
@@ -119,11 +162,18 @@ def admin_new():
             tempArticle =  Article.query.filter_by(title=title).first()
             # tags
 
-            tags = eval(request.form['tags'][1:-1])
-            print(tags)
-            print(type(tags))
+            ttags = eval(request.form['tags'][1:-1])
+            # print(ttags)
+            # print(type(ttags))
+            #标签只有一个，eval转出来是dict,有多个，eval转出来是tuple，转换成list再进行统一操作
+            tags = []
+            if isinstance(ttags, dict):
+                tags.append(ttags)
+            else:
+                tags = ttags
+            # 标签存在则查出，不存在则添加，最后添加文章-标签关系
             for tag in tags:
-                print('tag={}'.format(tag))
+                # print('tag={}'.format(tag))
                 tagName = tag['value']
                 tempTag = Tag.query.filter_by(name=tagName).first()
                 if tempTag is None:
@@ -140,5 +190,5 @@ def admin_new():
                 db.session.add(relation)
                 db.session.commit()
 
-            return jsonify({'status': True, 'msg': '发布成功！'})
+            return jsonify({'status': True, 'msg': '发布成功，点击跳转到文章详情页！', 'artid':tempArticle.id})
     
