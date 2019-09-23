aedaf223b1d55ac0b26579b6d44b9f920c89f4ce
lonisletend <yangxjsun0324@gmail.com>
2019-09-24 00:36:55 +0800

    更新分类表结构

--- app/models/article.py
+++ app/models/article.py
@@ -11,7 +11,7 @@ class Article(SearchableMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(256))
     text = db.Column(db.String(16000000))
-    cate_id = db.Column(db.Integer, db.ForeignKey('category.id'))
+    cate_id = db.Column(db.Integer)
     created = db.Column(db.DateTime, index=True, default=datetime.now)
     modified = db.Column(db.DateTime, index=True, default=datetime.now)
     is_topping = db.Column(db.Integer, index=True, default=0)  # 0=不置顶  1=置顶
--- app/models/category.py
+++ app/models/category.py
@@ -4,6 +4,7 @@ from app import db
 class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), index=True, unique=True)
-    articles = db.relationship('Article', backref='article', lazy='dynamic')
+    priority = db.Column(db.Integer)
+    description = db.Column(db.String(512))
     is_shown = db.Column(db.Integer, default=1)   # 1=展示  2=隐藏
     is_deleted = db.Column(db.Integer, default=0)
